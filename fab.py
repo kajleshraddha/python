378b8282b66cdd4b5a57b285759278db1910a636
12Shraddha <65213701+12Shraddha@users.noreply.github.com>
2020-10-15 15:56:48 +0530

    hacktoberfest

---
+++ fab.py
@@ -0,0 +1,11 @@
+
+def fav(n):
+    l=[0,1]
+    if n<=1:
+        return n
+    else:
+        for i in range(2,n+1):
+            l.append(l[i-2]+l[i-1])
+           
+    return l[-1]
+print(fav(int(input())))            
